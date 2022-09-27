dc88340e9cba5fdaa6371263bcfff5e66e9b3e06
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-09-27 14:52:39 -0400

    added update functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,6 @@
 // import modules
 import { toggleForm, clearForm } from "./form";
-import { initDb, getDb, postDb, deleteDb } from "./database";
+import { initDb, getDb, postDb, deleteDb, editDb } from "./database";
 import { fetchCards } from "./cards";
 // import CSS files
 import "../css/index.css";
@@ -64,4 +64,23 @@ window.deleteCard = (e) => {
   deleteDb(id);
   // Reload the DOM
   fetchCards();
+};
+
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+
+  // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
 };
